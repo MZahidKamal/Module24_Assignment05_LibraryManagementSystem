e339dba0e066e0e88b5c8c901180fd25bd041277
MZahidKamal <kamal.md.zahid@gmail.com>
2024-01-11 18:32:29 +0100

    Daily Learning & Commit

--- Module24_Assignment05_LibraryManagementSystem/settings.py
+++ Module24_Assignment05_LibraryManagementSystem/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-# ALLOWED_HOSTS = ['.vercel.app']
 # CSRF_TRUSTED_ORIGINS = ['https://library-b0fq.onrender.com','https://*.127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://library-b0fq.onrender.com','https://*.0.0.0.0']
 
